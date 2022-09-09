97082ca03a46dcc54dcdec15520e48daf84b94c6
chrispinciza01 <chrispinciza01@gmail.com>
2022-09-09 12:42:20 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /*
-*Programming is like building a multilingual puzzle
-*/
-
+ * Programming is like building a multilingual puzzle
+ */
 int main(void)
 {
 	char my_program[] = "Programming is like building a multilingual puzzle\n";
-	
+
 	puts(my_program);
-	return 0;
+	return (0);
+
 }
