02bab38dfd3275133e75dd7c0b0840ba958d134c
chrispin ciza <104005961+chrispinciza01@users.noreply.github.com>
2022-09-09 22:56:17 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,11 +4,7 @@
  */
 int main(void)
 {
-<<<<<<< HEAD
 	char my_program[] = "\"programming is like building a multilingual puzzle\n";
-=======
-	char my_program[] = "\"Programming is like building a multilingual puzzle\n";
->>>>>>> 316a561168f608cd6167431c363121a2a1e16481
 
 	puts(my_program);
 	return (0);
