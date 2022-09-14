d214a25adcfc5e4991f8d9f12db8ccac3b6d4642
chrispinciza01 <chrispinciza01@gmail.com>
2022-09-14 13:02:06 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalph - chechs for alphabetic character
+ * @c:the character to be checked
+ *return: for alphabetic character
+ */
+int _isalpha(int c)
+{
+
+
+	if ((c >= 65 && c <= 9) || (c >= 97 && c <= 122))
+
+	{
+		return (1);
+	}
+
+	return (0);
+}
